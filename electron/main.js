0e0eee53f0f7ce7ba76ebeed810d9e0cc4174ff4
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2026-02-05 15:49:38 +0600

    all file upload done

--- electron/main.js
+++ electron/main.js
@@ -1,33 +1,19 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
-let mainWindow;
-
 function createWindow() {
-  mainWindow = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: true,
-      contextIsolation: false,
     },
   });
 
-  // Load Next.js development server
-  mainWindow.loadURL("http://localhost:3000");
-
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+  // Load Next.js app
+  win.loadURL("http://localhost:3000"); // for dev
+  // For production:
+  // win.loadFile(path.join(__dirname, "../nextjs-app/out/index.html"));
 }
 
 app.whenReady().then(createWindow);
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
